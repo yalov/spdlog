24dde318fe034f0c7e809daa5a6e81dc85e1155a
Matteo Del Seppia <71389092+matteodelseppia@users.noreply.github.com>
2024-12-03 08:53:34 +0100

    Adding lock to rotate_now() (#3281)

--- include/spdlog/sinks/rotating_file_sink-inl.h
+++ include/spdlog/sinks/rotating_file_sink-inl.h
@@ -71,6 +71,7 @@ SPDLOG_INLINE filename_t rotating_file_sink<Mutex>::filename() {
 
 template <typename Mutex>
 SPDLOG_INLINE void rotating_file_sink<Mutex>::rotate_now() {
+    std::lock_guard<Mutex> lock(base_sink<Mutex>::mutex_);
     rotate_();
 }
 
